af237dcb229bf893c27425986f38cd27be878f6e
Joe-noh <goflb.jh@gmail.com>
2015-07-29 23:40:57 +0900

    it works

--- lib/mix/tasks/seed.ex
+++ lib/mix/tasks/seed.ex
@@ -1,48 +1,23 @@
 defmodule Mix.Tasks.Tane do
-  @shortdoc "seed!!"
-
   use Mix.Task
 
-  IO.inspect Code.require_file("mix.exs")
-  IO.inspect Mix.Project.config
-  @project_name Mix.Project.get |> Module.split |> List.first
+  @shortdoc "seed!!"
 
-  def run(opts \\ []) do
-    project_module = modulize(@project_name)
+  @default_path Path.join(~w[priv repo seeds])
 
-    repo_module = Keyword.get(opts, :repo, Repo)
-    repo_full_module = Module.concat(project_module, repo_module)
+  def run(args) do
+    {opts, _argv, _errors} = OptionParser.parse(args)
 
-    ["priv", Mix.Utils.underscore(repo_module), "seeds", "*.exs"]
+    path = Keyword.get(opts, :path, @default_path)
+
+    Mix.Task.run("app.start", [])
+    seed(path)
+  end
+
+  defp seed(path) do
+    [path, "*.exs"]
     |> Path.join
     |> Path.wildcard
-    |> Enum.map(&extract_seeds/1)
-    |> Enum.map(&insert_seeds(&1, repo_full_module))
-  end
-
-  defp extract_seeds(seed_file) do
-    {attrs, _bindings} = Code.eval_file(seed_file)
-    model_module = seed_file
-      |> Path.rootname
-      |> Path.basename
-      |> Mix.Utils.camelize
-      |> prepend_project_name
-      |> modulize
-
-    {model_module, attrs}
-  end
-
-  defp insert_seeds({model, seeds}, repo) do
-    Enum.map seeds, fn seed ->
-      apply(repo, :insert!, [struct(model, seed)])
-    end
-  end
-
-  defp prepend_project_name(module_name) do
-    Module.concat @project_name, module_name
-  end
-
-  defp modulize(string) do
-    Module.concat("Elixir", string)
+    |> Enum.each(&Code.eval_file/1)
   end
 end
--- lib/tane.ex
+++ lib/tane.ex
@@ -1,2 +1,22 @@
 defmodule Tane do
+  defstruct [:repo, :model]
+
+  def init do
+    %__MODULE__{}
+  end
+
+  def repo(tane, repo_module) do
+    %__MODULE__{tane | repo: repo_module}
+  end
+
+  def model(tane, model_module) do
+    %__MODULE__{tane | model: model_module}
+  end
+
+  def seed(tane = %__MODULE__{repo: repo_module, model: model_module}, row) do
+    model = struct(model_module, row)
+    apply(repo_module, :insert!, [model])
+
+    tane
+  end
 end
