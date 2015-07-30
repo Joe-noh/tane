9bf84c38c825dde854789e4cf47ba604337f4e9f
Joe-noh <goflb.jh@gmail.com>
2015-07-30 09:38:23 +0900

    add some docs and specs

--- lib/tane.ex
+++ lib/tane.ex
@@ -1,18 +1,36 @@
 defmodule Tane do
+  @moduledoc """
+  This module provides some functions to insert seeds.
+
+      import Tane
+
+      init
+      |> repo(MyApp.Repo)
+      |> model(MyApp.User)
+      |> seed(name: "Bob",  email: "bob@black.com")
+      |> seed(name: "Mary", email: "mary@blue.com")
+  """
+
+  @type t :: %__MODULE__{}
+
   defstruct [:repo, :model]
 
+  @spec init() :: t
   def init do
     %__MODULE__{}
   end
 
+  @spec repo(t, atom) :: t
   def repo(tane, repo_module) do
     %__MODULE__{tane | repo: repo_module}
   end
 
+  @spec model(t, atom) :: t
   def model(tane, model_module) do
     %__MODULE__{tane | model: model_module}
   end
 
+  @spec seed(t, Keyword.t) :: t
   def seed(tane = %__MODULE__{repo: repo_module, model: model_module}, row) do
     model = struct(model_module, row)
     apply(repo_module, :insert!, [model])
