3d9065f32750d8cf4cc52a485d87393526b78ec0
Joe-noh <goflb.jh@gmail.com>
2015-07-30 23:11:37 +0900

    accept map and keyword list

--- lib/tane.ex
+++ lib/tane.ex
@@ -30,8 +30,19 @@ defmodule Tane do
     %__MODULE__{tane | model: model_module}
   end
 
-  @spec seed(t, Keyword.t) :: t
-  def seed(tane = %__MODULE__{repo: repo_module, model: model_module}, row) do
+  @spec seed(t, map | Keyword.t) :: t
+  def seed(tane, row) when is_map(row) do
+    do_seed(tane, row)
+  end
+
+  def seed(tane, row) do
+    do_seed(tane, Enum.into(row, %{}))
+  end
+
+  @spec do_seed(t, map) :: t
+  defp do_seed(tane = %__MODULE__{repo: repo_module, model: model_module}, row) do
+    row = row |> Enum.into %{}
+
     model = struct(model_module, [])
     changeset = apply(model_module, :changeset, [model, row])
     apply(repo_module, :insert!, [changeset])
