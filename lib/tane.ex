4ee0eaa1a82dac36ef29881e6fe46fd92b3545d2
Joe-noh <goflb.jh@gmail.com>
2015-07-31 11:13:30 +0900

    implement delete_all! function

--- lib/tane.ex
+++ lib/tane.ex
@@ -30,6 +30,19 @@ defmodule Tane do
     %__MODULE__{tane | model: model_module}
   end
 
+  @spec delete_all!(t) :: t | no_return
+  def delete_all!(%__MODULE__{repo: nil}) do
+    raise ArgumentError, "repo is required to delete all entries"
+  end
+
+  def delete_all!(%__MODULE__{model: nil}) do
+    raise ArgumentError, "model is required to delete all entries"
+  end
+
+  def delete_all!(%__MODULE__{repo: repo, model: model}) do
+    apply(repo, :delete_all, [model])
+  end
+
   @spec seed(t, Keyword.t) :: t
   def seed(tane = %__MODULE__{repo: repo_module, model: model_module}, row) do
     model = struct(model_module, row)
