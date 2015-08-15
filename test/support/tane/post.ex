d694d24d84dbf809fb7f43930b8ff59ccd653fe6
Joe-noh <goflb.jh@gmail.com>
2015-08-15 12:42:32 +0900

    forgot alias

--- test/support/tane/post.ex
+++ test/support/tane/post.ex
@@ -1,6 +1,8 @@
 defmodule Tane.Post do
   use Ecto.Model
 
+  alias Tane.User
+
   schema "posts" do
     field :title, :string
     field :body,  :string
--- test/support/tane/user.ex
+++ test/support/tane/user.ex
@@ -1,11 +1,13 @@
 defmodule Tane.User do
   use Ecto.Model
 
+  alias Tane.Post
+
   schema "users" do
     field :name,  :string
     field :email, :string
 
-    has_many :posts, Post
+    has_many :posts, Post, on_delete: :delete_all
 
     timestamps
   end
